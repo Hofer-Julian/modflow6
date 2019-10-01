60a4125b463a882f633b4eddf025c1bcc96bae3f
mjr-deltares <martijn.russcher@deltares.nl>
2019-10-01 20:14:02 +0100

    Fill xy values for grid in GwfInterfaceModel - fill xy values for interface grid - add unit test

--- src/Model/GroundWaterFlow/gwf3disu8.f90
+++ src/Model/GroundWaterFlow/gwf3disu8.f90
@@ -1203,11 +1203,13 @@ module GwfDisuModule
     ! -- Allocate arrays in DISU
     call mem_allocate(this%idomain, this%nodes, 'IDOMAIN', this%origin)
     call mem_allocate(this%vertices, 2, this%nvert, 'VERTICES', this%origin)
-    if(this%nvert > 0) then
-      call mem_allocate(this%cellxy, 2, this%nodes, 'CELLXY', this%origin)
-    else
-      call mem_allocate(this%cellxy, 2, 0, 'CELLXY', this%origin)
-    endif
+    call mem_allocate(this%cellxy, 2, this%nodes, 'CELLXY', this%origin)
+    ! TODO_MJR: 
+    !if(this%nvert > 0) then
+    !  call mem_allocate(this%cellxy, 2, this%nodes, 'CELLXY', this%origin)
+    !else
+    !  call mem_allocate(this%cellxy, 2, 0, 'CELLXY', this%origin)
+    !endif
     !
     ! -- allocate connection object
     allocate(this%con)
